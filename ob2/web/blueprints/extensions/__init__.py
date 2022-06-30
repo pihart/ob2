061988149df5a23ccba5971aa720d0e538a6dd22
Rahul Kumar <rahulkumar@berkeley.edu>
2022-06-29 17:22:12 -0700

    send email

--- ob2/web/blueprints/extensions/__init__.py
+++ ob2/web/blueprints/extensions/__init__.py
@@ -3,12 +3,14 @@ import json
 import logging
 from flask import Blueprint, abort, request
 
+import ob2.config as config
 from ob2.database import DbCursor
 from ob2.database.helpers import create_build
 from ob2.dockergrader import dockergrader_queue, Job
 from ob2.util.github_api import get_commit_message, get_diff_file_list
 from ob2.util.hooks import apply_filters
 from ob2.util.job_limiter import rate_limit_fail_build, should_limit_source
+from ob2.mailer import create_email, mailer_queue
 
 blueprint = Blueprint("extensions", __name__, template_folder="templates")
 
@@ -33,8 +35,8 @@ def extensions():
         assert isinstance(assignment, str)
 
         with DbCursor() as c:
-            c.execute("SELECT sid FROM users WHERE login = ?", [login])
-            (db_sid,) = c.fetchone()
+            c.execute("SELECT name, sid, email FROM users WHERE login = ?", [login])
+            (name, db_sid,email) = c.fetchone()
 
             if sid != db_sid:
                 return ('Student ID in request does not match database', 400)
@@ -42,6 +44,11 @@ def extensions():
             c.execute("INSERT INTO extensions (user, assignment, days) VALUES (?, ?, ?)", [login, assignment, days])
             c.execute("SELECT last_insert_rowid()")
             (extension_id,) = c.fetchone()
+            if config.mailer_enabled:
+                email_payload = create_email("extension_confirm", email, "[CS 162] Extension Request Reviewed",
+                        days=days, assignment=assignment)
+                mailer_job = mailer_queue.create(c, "send", email_payload)
+                mailer_queue.enqueue(mailer_job)
 
         res = {}
         res["status"] = "OK"
